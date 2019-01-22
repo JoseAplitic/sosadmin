5cbb833287a3814cae19c4f5c2ef2dd712bae6af
JoseAplitic <jose@aplitic.com.gt>
2019-01-21 22:20:23 -0600

    Edicion del modulo script para instanciar la nueva librería JS

--- vistas/modulos/script.php
+++ vistas/modulos/script.php
@@ -5,4 +5,5 @@
 <script src="<?php echo SERVERURL; ?>vistas/js/sweetalert2.min.js"></script>
 <script src="<?php echo SERVERURL; ?>vistas/js/speakingurl.js"></script>
 <script src="<?php echo SERVERURL; ?>vistas/js/jquery.stringtoslug.min.js"></script>
+<script src="<?php echo SERVERURL; ?>vistas/js/chosen.jquery.min.js"></script>
 <script src="<?php echo SERVERURL; ?>vistas/js/main.js"></script>
